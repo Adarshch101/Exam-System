e7880f041568c3472b443b8b47acfe9db37db6d2
Adarshch101 <adchaudhari100@gmail.com>
2025-10-05 00:36:29 +0530

    some changes

--- server/src/index.js
+++ server/src/index.js
@@ -17,6 +17,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
+  'https://exam-system-omega.vercel.app',
   process.env.CORS_ORIGIN // still allow single-origin override
 ].filter(Boolean);
 
